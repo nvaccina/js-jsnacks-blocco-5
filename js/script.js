04e448da6a6cdaf39320bb82b3ccd5d9486c3447
nvaccina <niccolovac@gmail.com>
2023-04-04 14:41:44 +0200

    Snack-1

--- js/script.js
+++ js/script.js
@@ -3,3 +3,36 @@ jSnack 1
 Abbiamo un elenco utenti (nome, cognome, età).
 Trovare i minorenni e gli over 65
 */
+const users = [
+  {
+    firstname: 'Paolo',
+    lastname: 'Rossi',
+    age: 14,
+  },
+  {
+    firstname: 'Luigi',
+    lastname: 'Pieri',
+    age: 17,
+  },
+  {
+    firstname: 'Giovanni',
+    lastname: 'Trovato',
+    age: 32,
+  },
+  {
+    firstname: 'Marco',
+    lastname: 'Perso',
+    age: 70,
+  },
+  {
+    firstname: 'Lucia',
+    lastname: 'Rossi',
+    age: 20,
+  }
+]
+
+const minorenni = users.filter( (user) => user.age < 18)
+console.log(minorenni);
+
+const over65 = users.filter( (user) => user.age >= 65)
+console.log(over65);
