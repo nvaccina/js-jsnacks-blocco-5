fda44851bd78bfac3ee5f534201836213f96927b
nvaccina <niccolovac@gmail.com>
2023-04-05 14:26:33 +0200

    snack 4

--- js/script.js
+++ js/script.js
@@ -103,14 +103,11 @@ console.log('studentiOrdinati',studentiOrdinati);
 studentiOrdinati.forEach( (studente) => {
 
   const {firstname, lastname, matricola, voti} = studente;
+  
+  const sum = voti.reduce((tot, n) => tot + n);
+  const mediaVoti = sum / voti.length;
 
-  let somma = 0;
-  for(let i = 0; i < voti.length; i++){
-    somma += parseInt(voti[i]);
-  }
-  const mediaVoti = somma / voti.length;
-
-  const card =`
+  const card = `
     <div class="card mx-2 my-3" style="width: 14rem;">
       <img src="img/user.webp" class="card-img-top" alt="user">
       <div class="card-body">
@@ -125,3 +122,63 @@ studentiOrdinati.forEach( (studente) => {
 });
 
 
+/*
+*SNACK 4*
+Abbiamo un elenco degli studenti di una facoltà , identificati da _id_, _Nome_ e _somma totale_
+dei loro voti di esame...
+1. Per preparare l’aula di un nuovo corso, dobbiamo stampare le targhe col nome degli studenti:
+creare una lista contenente il loro nome tutto in maiuscolo
+ES (Marco della Rovere => MARCO DELLA ROVERE);
+2. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70
+3. Dobbiamo creare una lista di tutti gli studenti che hanno un totale di voti superiore a 70 e id
+superiore a 120
+*/
+
+const studentiFacolta = [
+  {
+    id: 213,
+    name: 'Marco della Rovere',
+    grade: 78,
+  },
+  {
+    id: 110,
+    name: 'Paola Cortellessa',
+    grade: 96,
+  },
+  {
+    id: 250,
+    name: 'Andrea Mantegna',
+    grade: 48,
+  },
+  {
+    id: 145,
+    name: 'Gaia Borromini',
+    grade: 74,
+  },
+  {
+    id: 196,
+    name: 'Luigi Grimaldello',
+    grade: 68,
+  },
+  {
+    id: 102,
+    name: 'Piero della Francesca',
+    grade: 50,
+  },
+  {
+    id: 120,
+    name: 'Francesca da Polenta',
+    grade: 84,
+  }
+]
+
+const nomeStudenti = studentiFacolta.map (studente => {
+  return `${studente.name}`.toUpperCase()
+})
+console.log(nomeStudenti);
+
+const studentiVotiMaggiori70 = studentiFacolta.filter( (studente) => studente.grade > 70);
+console.log(studentiVotiMaggiori70);
+
+const studentiVoti70Id120 = studentiVotiMaggiori70.filter( (studente) => studente.id >= 120);
+console.log(studentiVoti70Id120);
